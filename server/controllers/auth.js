e01d746cc40dadbf5f074176dbe442a7baf564ae
keple <keplerde@gmail.com>
2024-03-14 19:46:32 +0100

    routes&controllers

---
+++ server/controllers/auth.js
@@ -0,0 +1,7 @@
+const register = (req, res) => {
+    console.log('REGISTER ENDPOINT => ', req.body);
+
+  };
+  
+  module.exports = { register };
+  
--- server/server.js
+++ server/server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
+const fs = require('fs');
 require('dotenv').config();
 
 const app = express();
+
 // Connect to MongoDB
 mongoose.connect(process.env.DATABASE)
   .then(() => console.log('DB connected'))
   .catch(err => console.log('DB CONNECTION ERROR => ', err));
-
 
 // Middlewares
 app.use(express.json({ limit: '5mb' }));
@@ -17,12 +18,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({
   origin: 'http://localhost:3000',
 }));
+app.use(morgan('dev'));
 
-// Routes
-app.post('/api/register', (req, res) => {
-  console.log('REGISTER ENDPOINT => ', req.body);
-  // Registration logic here
-  res.json({ success: true });
+// Autoload routes
+fs.readdirSync('./routes').forEach((file) => {
+  // Make sure the file is a JavaScript file
+  if (file.endsWith('.js')) {
+    const route = require(`./routes/${file}`);
+    app.use('/api', route);
+  }
 });
 
 const port = process.env.PORT || 8000;
