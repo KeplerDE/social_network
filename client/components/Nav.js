30f46fadbfd8b18b25ba3ea4976788e76df330eb
keple <keplerde@gmail.com>
2024-03-25 21:37:46 +0100

    conditional_links

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,34 +1,35 @@
 import { useContext } from "react";
 import Link from "next/link";
 import { UserContext } from "../context";
-import { useRouter } from 'next/router';
-
+import { useRouter } from "next/router";
 
 const Nav = () => {
   const { state, setState } = useContext(UserContext);
-
-
   const router = useRouter();
 
   const logout = () => {
     window.localStorage.removeItem('auth');
     setState(null);
-    router.push("/login")
-  }
+    router.push("/login");
+  };
+
   return (
     <nav className="nav bg-dark d-flex justify-content-between">
-      <Link href="/" className="nav-link text-light logo">
-        HOME
-      </Link>
-      <Link href="/login" className="nav-link text-light"> 
-        Login
-      </Link>
-      <Link href="/register" className="nav-link text-light">
-        Register
-      </Link>
-      <a onClick={logout} className="nav-link text-light">
-        Logout
-      </a>
+      <Link href="/" className="nav-link text-light logo">HOME</Link>
+      {state !== null ? (
+        <a onClick={logout} className="nav-link text-light">
+          Logout
+        </a>
+      ) : (
+        <>
+          <Link href="/login" className="nav-link text-light">
+            Login
+          </Link>
+          <Link href="/register" className="nav-link text-light">
+            Register
+          </Link>
+        </>
+      )}
     </nav>
   );
 };
