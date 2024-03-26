8ceb8141c83391254901008067e5f2a1b09fee3e
keple <keplerde@gmail.com>
2024-03-26 23:23:23 +0100

    active_links

--- client/components/Nav.js
+++ client/components/Nav.js
@@ -1,25 +1,43 @@
-import { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Link from "next/link";
 import { UserContext } from "../context";
 import { useRouter } from "next/router";
 
 const Nav = () => {
+  const [current, setCurrent] = useState("");
   const { state, setState } = useContext(UserContext);
+
+  useEffect(() => {
+    if (process.browser) {
+      setCurrent(window.location.pathname);
+    }
+  }, [process.browser && window.location.pathname]);
+
+  console.log('current.. ', current)
+
   const router = useRouter();
 
   const logout = () => {
-    window.localStorage.removeItem('auth');
+    window.localStorage.removeItem("auth");
     setState(null);
     router.push("/login");
   };
 
+
   return (
     <nav className="nav bg-dark d-flex justify-content-between">
-      <Link href="/" className="nav-link text-light logo">HOME</Link>
+      <Link href="/" className={`nav-link text-light logo ${current === "/" && "active"}`}>HOME</Link>
       {state !== null ? (
+        <>
+        <Link href="/user/dashboard" className={`nav-link text-light ${current === "/user/dashboard" && "active"}`}>
+
+          { state && state.user && state.user.name}
+        </Link>
+
         <a onClick={logout} className="nav-link text-light">
           Logout
         </a>
+        </>
       ) : (
         <>
           <Link href="/login" className="nav-link text-light">
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,24 +1,21 @@
 import { UserProvider } from "../context";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from 'next/app';
 import Nav from "../components/Nav";
 import Head from 'next/head';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
-
 function MyApp({ Component, pageProps }) {
-    return (
-      <UserProvider>
-        <Head>
-          <link rel="stylesheet" href="/css/styles.css" />
-        </Head>
-        <Nav />
-        <ToastContainer position="top-center" />
-        <Component {...pageProps} />
-      </UserProvider>
-    );
-  }
-  
-  export default MyApp;
+  return (
+    <UserProvider>
+      <Head>
+        <link rel="stylesheet" href="/css/styles.css" />
+      </Head>
+      <Nav />
+      <ToastContainer position="top-center" />
+      <Component {...pageProps} />
+    </UserProvider>
+  );
+}
+
+export default MyApp;
