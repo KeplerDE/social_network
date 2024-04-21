a54c6195822f8a585998f60d1b1c052cb515360d
keple <keplerde@gmail.com>
2024-04-21 16:07:12 +0200

    react_quill

--- client/components/forms/CreatePostForm.js
+++ client/components/forms/CreatePostForm.js
@@ -1,22 +1,27 @@
+import React from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css'; 
+
 const CreatePostForm = ({ content, setContent, postSubmit }) => {
     return (
         <div className="card">
             <div className="card-body pb-3">
                 <form className="form-group">
-                    <textarea 
+
+                    <ReactQuill 
+                        theme="snow" 
                         value={content}
-                        onChange={(e) => setContent(e.target.value)}
-                        className="form-control"
+                        onChange={setContent} // ReactQuill уже предоставляет чистый HTML, поэтому нет необходимости использовать e.target.value
                         placeholder="Write something..."
-                    ></textarea>
+                    />
                 </form>
             </div>
 
             <div className="card-footer">
                 <button 
-                disabled={!content}
-                onClick={postSubmit} 
-                className="btn btn-primary btn-sm mt-1">Post</button>
+                    disabled={!content}
+                    onClick={postSubmit} 
+                    className="btn btn-primary btn-sm mt-1">Post</button>
             </div>
         </div>
     );
